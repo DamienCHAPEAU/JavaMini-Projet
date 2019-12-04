89d55ce066b375060de1eb8de7e8f345cdeb39e8
damien.chapeau@etud.univ-jfc.fr <>
2019-12-04 14:33:36 +0100

    Ajout fonction chiffre d'affaire au DAO

---
+++ src/main/java/DAO/ChiffreAffaire.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author pedago
+ */
+public class ChiffreAffaire {
+    
+    private float CA;
+
+    public ChiffreAffaire(float CA) {
+        this.CA = CA;
+    }
+
+    public float getCA() {
+        return CA;
+    }
+    
+    
+    
+}
--- src/main/java/DAO/DAO.java
+++ src/main/java/DAO/DAO.java
@@ -184,5 +184,83 @@ public class DAO {
         }
         return result;
     }
+    
+    //Requete visualiser les chiffres d'affaire par pays, en choisissant la période (date de début / date de fin) sur laquelle doit porter la statistique.
+        //Exemple :
+        //SELECT SUM(PORT) AS CA FROM COMMANDE WHERE PAYS_LIVRAISON='France' AND SAISIE_LE>='1994-08-08' AND ENVOYEE_LE<='1994-08-15';
+        
+        public ChiffreAffaire caByPays(String pays, String dateSaisie, String dateEnvoyee) throws SQLException, DAOException {
+
+		ChiffreAffaire result = new ChiffreAffaire(0);
+
+		String sql = "SELECT SUM(PORT) FROM COMMANDE WHERE PAYS_LIVRAISON=(?) AND SAISIE_LE>=(?) AND ENVOYEE_LE<=(?)";
+		try (Connection connection = myDataSource.getConnection(); 
+		     PreparedStatement stmt = connection.prepareStatement(sql)) {
+                        stmt.setString(1, pays);
+                        stmt.setString(2, dateSaisie);
+                        stmt.setString(3, dateEnvoyee);
+                        try (ResultSet rs = stmt.executeQuery()) {
+				if (rs.next()) { // On a trouvé					                                      
+                                        ChiffreAffaire ca = new ChiffreAffaire(rs.getFloat("CA"));
+                                        result = ca;
+				} 
+			} catch (SQLException ex) {
+			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+			throw new DAOException(ex.getMessage());
+                        }                       
+			
+		}
+		return result;
+	}
+        
+        
+        /*public ChiffreAffaire caByCategorie(int categorie, String dateSaisie, String dateEnvoyee) throws SQLException, DAOException {
+
+		ChiffreAffaire result = new ChiffreAffaire(0);
+
+		String sql = "A FAIRE";
+		try (Connection connection = myDataSource.getConnection(); 
+		     PreparedStatement stmt = connection.prepareStatement(sql)) {
+                        stmt.setInt(1, categorie);
+                        stmt.setString(2, dateSaisie);
+                        stmt.setString(3, dateEnvoyee);
+                        try (ResultSet rs = stmt.executeQuery()) {
+				if (rs.next()) { // On a trouvé					                                      
+                                        ChiffreAffaire ca = new ChiffreAffaire(rs.getFloat("CA"));
+                                        result = ca;
+				} 
+			} catch (SQLException ex) {
+			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+			throw new DAOException(ex.getMessage());
+                        }                       
+			
+		}
+		return result;
+	}*/
+        
+        public ChiffreAffaire caByClient(String client, String dateSaisie, String dateEnvoyee) throws SQLException, DAOException {
+
+		ChiffreAffaire result = new ChiffreAffaire(0);
+
+		String sql = "SELECT SUM(PORT) FROM COMMANDE WHERE CLIENT=(?) AND SAISIE_LE>=(?) AND ENVOYEE_LE<=(?)";
+		try (Connection connection = myDataSource.getConnection(); 
+		     PreparedStatement stmt = connection.prepareStatement(sql)) {
+                        stmt.setString(1, client);
+                        stmt.setString(2, dateSaisie);
+                        stmt.setString(3, dateEnvoyee);
+                        try (ResultSet rs = stmt.executeQuery()) {
+				if (rs.next()) { // On a trouvé					                                      
+                                        ChiffreAffaire ca = new ChiffreAffaire(rs.getFloat("CA"));
+                                        result = ca;
+				} 
+			} catch (SQLException ex) {
+			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+			throw new DAOException(ex.getMessage());
+                        }                       
+			
+		}
+		return result;
+	}
+        
 
 }
